aa157cecb892df3f3884178d8157d547dbe88cd7
Millena Engeroff <141692137+MillenaEngeroff@users.noreply.github.com>
2026-08-19 11:01:46 -0300

    Add files via upload

---
+++ funcoes/multiplas_soma.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exibeMultiplosDeSoma (int vetor [], int quantidade, int soma)
+{
+    int encontrouMultiplo=0;
+    printf("\nElementos multiplos da soma:\n");
+
+    for (int i=0,i<quantidade;i++)
+    {
+        if (soma!=)
+    }
+
+}
+
+int main()
+{
+    int vetor[5];
+   int soma=0;
+    printf("Informe 8 elementos:\n");
+    for (int i=0; i<8; i++);
+    {
+        do
+        {
+            printf("Elemento %d:\n",i+1);
+            scanf("%d",&vetor[i]);
+
+            if (vetor[i]<=0)
+            {
+                printf("Tente novamente!");
+            }
+        }
+
+        while (vetor[i] <=0)
+            soma+=vetor[i];
+
+    }
+
+
+
+    return 0;
+}
