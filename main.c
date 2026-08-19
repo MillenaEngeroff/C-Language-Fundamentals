6b66a82d0bcc7d6645086c9db6966960163c7824
Millena Engeroff <141692137+MillenaEngeroff@users.noreply.github.com>
2026-08-19 10:08:15 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>// Imformar se é par ou impar
+
+int main()
+{
+    int num;
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("Informe um valor: ");
+    scanf ("%d", &num);
+    if (num%2==0)
+    {
+        printf("O número é par");
+    }
+   else
+    {
+       printf("O número é impar");
+    }
+    return 0;
+}
